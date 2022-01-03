250e6849882c0fd96af358eeb6299a506c4c9358
Christian Gabrielsson <christiangabrielsson@Christians-MacBook-Pro-2.local>
2022-01-02 21:29:12 -0700

    temporarily disabled marquee scraping

--- web-scraping/fetch-data-and-update-collection.js
+++ web-scraping/fetch-data-and-update-collection.js
@@ -2,20 +2,25 @@ const EdmEvent = require('../models/edmevent');
 const fetchZouk = require('./fetch-zouk')
 const fetchHakassanGroup = require('./fetch-hakassan-group')
 const fetchWynn = require('./fetch-wynn')
-const fetchMarquee = require('./fetch-marquee')
+// const fetchMarquee = require('./fetch-marquee') 
+
+// Need to fix marquee scraping so it works reliably
+// need to create fail safes so that if one script it does not bring other scraping to a stop
+// create logging so that i get notified if it fails scrapping
+
 
 async function fetchDataAndUpdateCollection() {
     try {
       const zoukEdmEvents = await fetchZouk.fetchZoukEDMEvents()
       const hakkasanGroupEdmEvents = await fetchHakassanGroup.getHakassanGroupEDMEvents()
       const wynnEdmEvents = await fetchWynn.fetchWynnGroupEDMEvents()
-      const marqueeEdmEvents = await fetchMarquee.fetchMarqueeEDMEvents()
+      // const marqueeEdmEvents = await fetchMarquee.fetchMarqueeEDMEvents()
       const _collectionDeleted = await EdmEvent.deleteMany()
       const _resultFromCollection = await EdmEvent.insertMany([
         ...zoukEdmEvents, 
         ...hakkasanGroupEdmEvents, 
         ...wynnEdmEvents, 
-        ...marqueeEdmEvents
+        // ...marqueeEdmEvents
       ])
       console.log('fetching data and updating the Collection has been completed Successfully')
     } catch(error) {
