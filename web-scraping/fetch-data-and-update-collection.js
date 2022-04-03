bf5a50929473d4fbc79e4b897b15c405b06335b8
Christian Gabrielsson <christiangabrielsson@Christians-MacBook-Pro-2.local>
2022-04-03 13:35:50 -0700

    fixed zouk issue and deprecated marquee script

--- web-scraping/fetch-data-and-update-collection.js
+++ web-scraping/fetch-data-and-update-collection.js
@@ -2,7 +2,6 @@ const EdmEvent = require('../models/edmevent');
 const fetchZouk = require('./fetch-zouk')
 const fetchHakassanGroup = require('./fetch-hakassan-group')
 const fetchWynn = require('./fetch-wynn')
-const fetchMarquee = require('./fetch-marquee') 
 const SendEmail = require("../utilities/send-email-alert")
 
 async function fetchDataAndUpdateCollection() {
@@ -10,13 +9,11 @@ async function fetchDataAndUpdateCollection() {
       const zoukEdmEvents = await fetchZouk.fetchZoukEDMEvents()
       const hakkasanGroupEdmEvents = await fetchHakassanGroup.getHakassanGroupEDMEvents()
       const wynnEdmEvents = await fetchWynn.fetchWynnGroupEDMEvents()
-      const marqueeEdmEvents = await fetchMarquee.fetchMarqueeEDMEvents()
       const _collectionDeleted = await EdmEvent.deleteMany()
       const _resultFromCollection = await EdmEvent.insertMany([
         ...zoukEdmEvents, 
         ...hakkasanGroupEdmEvents, 
-        ...wynnEdmEvents, 
-        ...marqueeEdmEvents
+        ...wynnEdmEvents
       ])
       console.log('Fetching data and updating the EDM Events  Collection has been completed Successfully')
     } catch(error) {
